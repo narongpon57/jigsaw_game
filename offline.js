081c0521494347d00e7fd137afc3802d2442ac9a
Narongpon Suthimakphol <narongpon.dev@gmail.com>
2020-05-13 20:21:20 +0700

    init

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1589375634,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bggame-sheet0.png",
+		"images/jigsaw-sheet0.png",
+		"images/pic-sheet0.png",
+		"images/canvas.png",
+		"images/particles.png",
+		"media/5c3c9e2f9c065.m4a",
+		"media/5c3c9e2f9c065.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
